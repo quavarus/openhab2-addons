e1579408861907176a612d7ec8d337d0fc2b2360
quavarus <quavarus@gmail.com>
2016-08-16 22:34:07 -0400

    smartthings initial commit

---
+++ addons/binding/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/config/SmartThingsBridgeConfiguration.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2014-2015
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.smartthings.config;
+
+/**
+ * The {@link SmartThingsBridgeConfiguration} is responsible for holding configuration
+ * informations needed to access SmartThings OpenHAB SmartApp
+ *
+ * @author quavarus - Initial contribution
+ */
+public class SmartThingsBridgeConfiguration {
+    public String clientId;
+    public String clientSecret;
+    public String username;
+    public String password;
+}
